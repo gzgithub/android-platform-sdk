411c39bd00dd8b8ffa92a8db6e53fd49d65fcbeb
Chris Craik <ccraik@google.com>
2016-04-21 16:40:28 -0700

    Account for new systrace html asset file path
    bug:27860073
    
    Change-Id: Ib17964036acffaaf0b1dd25c92c58055e754a261

--- eclipse/plugins/com.android.ide.eclipse.ddms/src/com/android/ide/eclipse/ddms/views/DeviceView.java
+++ eclipse/plugins/com.android.ide.eclipse.ddms/src/com/android/ide/eclipse/ddms/views/DeviceView.java
@@ -567,8 +567,22 @@ public class DeviceView extends ViewPart implements IUiSelectionListener, IClien
         }
     };
 
+    private final String[] mAssetSubdirPath = new String[] { "catapult", "systrace", "systrace" };
     private void launchSystrace(final IDevice device, final Shell parentShell) {
-        final File systraceAssets = new File(DdmsPlugin.getPlatformToolsFolder(), "systrace"); //$NON-NLS-1$
+        File rootSystraceAssets = new File(DdmsPlugin.getPlatformToolsFolder(),
+                "systrace"); //$NON-NLS-1$
+        File systraceAssets = rootSystraceAssets;
+        for (String subdir : mAssetSubdirPath) {
+            File subDir = new File(systraceAssets, subdir);
+            if (subDir.isDirectory()) {
+                systraceAssets = subDir;
+            } else {
+                // new file layout not observed, abort
+                systraceAssets = rootSystraceAssets;
+                break;
+            }
+        }
+
         if (!systraceAssets.isDirectory()) {
             MessageDialog.openError(parentShell, "Systrace",
                     "Updated version of platform-tools (18.0.1 or greater) is required.\n"
