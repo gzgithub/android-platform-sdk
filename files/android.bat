e88c4c7db25d8983022a093cc8097f95de7c2bf9
Joe Baker-Malone <jbakermalone@google.com>
2017-02-27 16:47:02 -0800

    Adding tombstone for android
    Change-Id: I5916f72de8f8334c1755fea324ecca7d9d4eb660

---
+++ files/android.bat
@@ -0,0 +1,6 @@
+@echo off
+echo The "android" command is no longer available.
+echo For manual SDK and AVD management, please use Android Studio.
+echo For command-line tools, use
+echo tools\bin\sdkmanager.bat and tools\bin\avdmanager.bat
+exit /b 1
